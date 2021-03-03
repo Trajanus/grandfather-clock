106dcc1ba2925ecd8a9c312575effd186cc6717b
Trajanus <derektgoodwin@gmail.com>
2021-03-03 13:31:57 -0800

    initial commit of dotnet core console application with topshelf extension and tutorial example.

---
+++ GrandfatherClock/ClockService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace GrandfatherClock
+{
+    public class ClockService : IHostedService
+    {
+        readonly Timer _timer;
+        public ClockService()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public Task StartAsync(System.Threading.CancellationToken cancellationToken) { _timer.Start(); return Task.CompletedTask; }
+        public Task StopAsync(System.Threading.CancellationToken cancellationToken) { _timer.Stop(); return Task.CompletedTask; }
+    }
+}
---
+++ GrandfatherClock/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Topshelf.Extensions.Hosting;
+
+namespace GrandfatherClock
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateHostBuilder(args);
+
+            builder.RunAsTopshelfService(hc =>
+            {
+                hc.SetServiceName("GenericHostInTopshelf");
+                hc.SetDisplayName("Generic Host In Topshelf");
+                hc.SetDescription("Runs a generic host as a Topshelf service.");
+            });
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<ClockService>();
+                });
+        }
+    }
+}
