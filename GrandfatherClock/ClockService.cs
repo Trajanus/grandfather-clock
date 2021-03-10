a6ec1e68dbc5f1ce1207f94bcb5fb7f9686f5b43
Trajanus <derektgoodwin@gmail.com>
2021-03-09 16:16:50 -0800

    Chime file now loaded into memory once and used for playback instead of loading file during each use.  Chime directory is now created if not found.

--- GrandfatherClock/ClockService.cs
+++ GrandfatherClock/ClockService.cs
@@ -2,6 +2,7 @@
 using NAudio.Wave;
 using Serilog;
 using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Timers;
 
@@ -12,19 +13,39 @@ namespace GrandfatherClock
         private const int MillisecondsInHour = 3600000;
         private const int MillisecondsInFiveMinutes = 300000;
 
+        private readonly string _chimeFolderPath;
         private readonly int _millisecondsFactor;
         private readonly Timer _timer;
+
+        private RawSourceWaveStream _chime;
+
         public ClockService(int chimeIntervalInMilliseconds)
         {
             _millisecondsFactor = chimeIntervalInMilliseconds;
             _timer = new Timer(GetMillisecondsToNextChime(_millisecondsFactor)) { AutoReset = true };
             Log.Information($"Next chime will be at {DateTime.Now.AddMilliseconds(_timer.Interval)}");
             _timer.Elapsed += Chime;
+            _chimeFolderPath = $@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\GrandfatherClock\chimes\";
         }
-        public Task StartAsync(System.Threading.CancellationToken cancellationToken) 
+        public async Task StartAsync(System.Threading.CancellationToken cancellationToken) 
         {
+            if (!Directory.Exists(_chimeFolderPath))
+            {
+                Log.Warning($"Chime folder {_chimeFolderPath} does not exist, it will be created.");
+                Directory.CreateDirectory(_chimeFolderPath);
+            }
+
+            try
+            {
+                _chime = await LoadChime($"{_chimeFolderPath}chime.mp3");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Log.Error($"Failed to find chime file at {_chimeFolderPath}chime.mp3", ex);
+            }
+
             _timer.Start(); 
-            return Task.CompletedTask; 
+            return; 
         }
         public Task StopAsync(System.Threading.CancellationToken cancellationToken) 
         { 
@@ -33,45 +54,52 @@ namespace GrandfatherClock
             return Task.CompletedTask; 
         }
 
+        private async Task<RawSourceWaveStream> LoadChime(string chimeFilePath)
+        {
+            RawSourceWaveStream chime;
+            Mp3WaveFormat format;
+            MemoryStream stream = new MemoryStream();
+
+            using (var audioFile = File.OpenRead(chimeFilePath))
+            using (var mp3 = new Mp3FileReader(audioFile))
+            {
+                format = mp3.Mp3WaveFormat;
+                await audioFile.CopyToAsync(stream);
+            }
+
+            chime = new RawSourceWaveStream(stream, format);
+            chime.Position = 0;
+
+            return chime;
+        }
+
         private void Chime(object sender, ElapsedEventArgs eventArgs)
         {
             Timer timer = (Timer)sender;
             timer.Interval = GetMillisecondsToNextChime(_millisecondsFactor);
             Log.Information($"Next chime will be at {DateTime.Now.AddMilliseconds(timer.Interval)}");
 
-            PlayChime($@"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\GrandfatherClock\chimes\chime.mp3");
+            PlayChime(_chime);
         }
 
-        private void PlayChime(string chimeFilePath)
+        private void PlayChime(RawSourceWaveStream chime)
         {
-            try
+            if(null == chime)
             {
-                using (var audioFile = new Mp3FileReader(chimeFilePath))
+                Console.Beep();
+            }
+            else
+            {
                 using (var outputDevice = new WaveOutEvent())
                 {
-                    outputDevice.Init(audioFile);
+                    outputDevice.Init(chime);
                     outputDevice.Play();
                     while (outputDevice.PlaybackState == PlaybackState.Playing)
                     {
                         Task.Delay(1000);
                     }
                 }
-            }
-            catch(Exception ex)
-            {
-                if(ex is System.IO.FileNotFoundException
-                    || ex is System.IO.DirectoryNotFoundException)
-                {
-                    Log.Error($"Failed to find chime file at {chimeFilePath}", ex);
-                    Console.Beep();
-                }
-                else
-                {
-                    Log.Error("Chime playback failed.", ex);
-                    Console.Beep();
-                    throw;
-                }
-                
+                chime.Position = 0;
             }
         }
 
