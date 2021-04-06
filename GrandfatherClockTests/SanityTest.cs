f0ec38b6dd4a71a4e4b85feadc4ee08bac40f86f
Trajanus <derektgoodwin@gmail.com>
2021-04-05 19:23:59 -0700

    added xunit and sanity test.

---
+++ GrandfatherClockTests/SanityTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GrandfatherClockTest
+{
+    public class SanityTests
+    {
+        [Fact]
+        public void TestFrameworkWorking_True_AssetsTrue()
+        {
+            Assert.True(true);
+        }
+    }
+}
